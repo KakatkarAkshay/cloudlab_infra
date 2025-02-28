f07d78b148ee881cba0c3d2bc2a8b4eda149097c
Akshay Kakatkar <akshayakakatkaraa@gmail.com>
2025-02-28 14:41:15 +0000

    Add OCI Provider

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+      version = "6.28.0"
+    }
+  }
+}
+
+provider "oci" {
+    user_ocid = var.oci_user  
+    fingerprint = var.oci_fingerprint
+    private_key = var.oci_private_key
+    tenancy_ocid = var.oci_tenancy
+    region = var.oci_region
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "oci_user" {
+    type = string
+    description = "OCI user"
+}
+
+variable "oci_fingerprint" {
+    type = string
+    description = "OCI fingerprint" 
+}
+
+variable "oci_tenancy" {
+    type = string
+    description = "OCI tenancy"
+}
+
+variable "oci_region" {
+    type = string
+    description = "OCI region"
+}
+
+variable "oci_private_key" {
+    type = string
+    description = "OCI private key"
+}
