4be4b93c46e539f16cc9d536ee236a1970736ca1
Akshay Kakatkar <akshayakakatkaraa@gmail.com>
2025-03-02 13:47:15 +0000

    Initialize OCI Provider

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "oci_user" {
+    type = string
+    description = "OCI user"
+}
+
+variable "oci_fingerprint" {
+    type = string
+    description = "OCI fingerprint" 
+}
+
+variable "oci_tenancy" {
+    type = string
+    description = "OCI tenancy"
+}
+
+variable "oci_region" {
+    type = string
+    description = "OCI region"
+}
+
+variable "oci_private_key" {
+    type = string
+    description = "OCI private key"
+}
+
+variable "oci_compartment" {
+    type = string
+    description = "OCI compartment"
+}
