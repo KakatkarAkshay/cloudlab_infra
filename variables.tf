db4b463f520f65d6447cfe514489767473a32e68
Akshay Kakatkar <akshayakakatkaraa@gmail.com>
2025-03-13 09:51:48 +0000

    Mark variables sensitive

--- variables.tf
+++ variables.tf
@@ -1,26 +1,31 @@
 variable "oci_tenancy_1_user" {
   type        = string
   description = "OCI user"
+  sensitive   = true
 }
 
 variable "oci_tenancy_2_user" {
   type        = string
   description = "OCI user"
+  sensitive   = true
 }
 
 variable "oci_tenancy_1" {
   type        = string
   description = "OCI tenancy"
+  sensitive   = true
 }
 
 variable "oci_tenancy_2" {
   type        = string
   description = "OCI tenancy"
+  sensitive   = true
 }
 
 variable "oci_fingerprint" {
   type        = string
   description = "OCI fingerprint"
+  sensitive   = true
 }
 
 variable "oci_region" {
@@ -31,6 +36,7 @@ variable "oci_region" {
 variable "oci_private_key" {
   type        = string
   description = "OCI private key"
+  sensitive   = true
 }
 
 variable "oci_instance_shape" {
@@ -42,4 +48,5 @@ variable "oci_instance_shape" {
 variable "tailscale_token" {
   type        = string
   description = "Tailscale token"
+  sensitive   = true
 }
